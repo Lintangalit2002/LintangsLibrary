12f77ab33ded89c0dae97febb3d3c37d7c6832c6
Lintangalit2002 <lintangalit2002@gmail.com>
2025-08-30 01:04:34 +0200

    created react vite project

---
+++ lintangs-library/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <body>
+        This is body text
+      </body>
+      <h1>
+        This is h1 text
+      </h1>
+    </>
+  )
+}
+
+export default App;
